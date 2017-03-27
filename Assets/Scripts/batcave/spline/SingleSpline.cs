3cf45727030197245e4a1c4de045c28492ac0ab6
nice-shot <shefferu@gmail.com>
2017-03-27 20:43:02 +0300

    Started writing the single spline code. Didn't really understand how to do it...

--- Assets/Scripts/batcave/spline/SingleSpline.cs
+++ Assets/Scripts/batcave/spline/SingleSpline.cs
@@ -2,7 +2,11 @@
 
 namespace BatCave.Spline {
 public class SingleSpline {
+    
+    private Vector2[] controlPoints;
+
     public SingleSpline(Vector2[] controlPoints) {
+        this.controlPoints = controlPoints;
         // TODO: Implement: initialize the spline to match the given control points.
     }
 
@@ -12,7 +16,20 @@ public class SingleSpline {
     public float Value(float X) {
         // TODO: Implement: find the polynom f_i that passes at X and calculate
         //       f_i(X).
+
         return 0f;
+    }
+
+    private float GetA(Vector2 point1, Vector2 point2, float derivative) {
+        return derivative * (point2.x - point1.x) - (point2.y - point1.y);
+    }
+
+    private float GetB(Vector2 point1, Vector2 point2, float derivative) {
+        return (-derivative) * (point2.x - point1.x) + (point2.y - point1.y);
+    }
+
+    private float GetT(Vector2 point1, Vector2 point2, float X) {
+        return (X - point1.x) / (point2.x - point1.x);
     }
 }
 }
