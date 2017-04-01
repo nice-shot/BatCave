ff7f294616a77e30ebb8dca8078976e637101434
nice-shot <shefferu@gmail.com>
2017-04-01 11:28:17 +0300

    Added the basis for a pattern system in the TerrainGenerator

--- Assets/Scripts/batcave/terrain/TerrainGenerator.cs
+++ Assets/Scripts/batcave/terrain/TerrainGenerator.cs
@@ -2,6 +2,59 @@
 using Infra.Collections;
 
 namespace BatCave.Terrain {
+// Not points but small ranges to create points within. This makes the repeating
+// patterns a bit different from one another
+[System.Serializable]
+public class TerrainPointPrepare {
+    // Range for distance to previous point
+    public float distanceMin;
+    public float distanceMax;
+    // Range for ceiling height
+    public float ceilingMin;
+    public float ceilingMax;
+    // Range for floor height
+    public float floorMin;
+    public float floorMax;
+
+    public float GetDistance() {
+        return Random.Range(distanceMin, distanceMax);
+    }
+
+    public float GetCeilingY() {
+        return Random.Range(ceilingMin, ceilingMax);
+    }
+
+    public float GetFloorY() {
+        return Random.Range(floorMin, floorMax);
+    }
+       
+}
+
+// Holds a group of points to create a specific pattern
+[System.Serializable]
+public class TerrainPattern {
+    public string name;
+    public TerrainPointPrepare[] points;
+    private int currentIndex = 0;
+
+    private void ResetPoints() {
+        currentIndex = 0;
+    }
+
+    public bool HasMorePoints() {
+        return (currentIndex < points.Length);
+    }
+
+    public TerrainPointPrepare GetNextPointPrepare() {
+        if (!HasMorePoints()) {
+            ResetPoints();
+        }
+        var point = points[currentIndex];
+        currentIndex++;
+        return point;
+    }
+}
+
 public class TerrainGenerator : MonoSingleton<TerrainGenerator> {
     [System.Serializable]
     public class TerrainPoint {
@@ -33,7 +86,16 @@ public class TerrainGenerator : MonoSingleton<TerrainGenerator> {
     [Tooltip("The X position of the first point")]
     public float startingX;
 
+    public TerrainPattern[] terrainPatterns;
+
     private readonly ObjectPool<TerrainPoint> terrainPoints = new ObjectPool<TerrainPoint>(5, 5);
+    private TerrainPattern currentPattern;
+       
+
+    // Sets the current pattern to the first which should be the wide tunnel
+    protected void Awake() {
+        currentPattern = terrainPatterns[0];
+    }
 
     /// <summary>
     /// Returns the next point according to the difficulty level.
@@ -44,30 +106,55 @@ public class TerrainGenerator : MonoSingleton<TerrainGenerator> {
         return instance._GetNextPoint(difficulty);
     }
 
+//    private TerrainPoint _GetNextPoint(int difficulty) {
+//        var point = terrainPoints.Borrow();
+//        point.difficulty = difficulty;
+//
+//        // EXERCISE: Difficulty should affect distance from previous point,
+//        //           height differences from previous point (slope) and gap
+//        //           between floor and ceiling.
+//        //           Need to take into account previous terrain to make sure the
+//        //           terrain is passable: don’t create tunnels that are too
+//        //           narrow or slopes that are too steep for the bat to maneuver.
+//
+//        if (difficulty == 0) {
+//            // Create a nice random tunnel that is very wide.
+//            // Note that difficulty is 0 before the game starts, so this should
+//            // never collide with the bat that is flying at the center of the
+//            // cave.
+//            point.floorY = Random.Range(minFloor, minFloor + 0.5f);
+//            point.ceilingY = Random.Range(maxCeiling - 0.5f, maxCeiling);
+//        } else {
+//            point.floorY = Random.Range(minFloor, maxCeiling - minGap);
+//            point.ceilingY = Random.Range(point.floorY + minGap, maxCeiling);
+//        }
+//        // Choose the distance from the previous point.
+//        point.distanceFromPrevious = Random.Range(minDistance, maxDistance);
+//
+//        // Set the point at the correct position based on the previous point's
+//        // position and the distance from it that we just set.
+//        if (Game.instance.terrainPoints.Count == 0) {
+//            point.x = startingX;
+//        } else {
+//            float previousX = Game.instance.terrainPoints[Game.instance.terrainPoints.Count - 1].x;
+//            point.x = previousX + point.distanceFromPrevious;
+//        }
+//
+//        return point;
+//    }
+
     private TerrainPoint _GetNextPoint(int difficulty) {
         var point = terrainPoints.Borrow();
         point.difficulty = difficulty;
 
-        // EXERCISE: Difficulty should affect distance from previous point,
-        //           height differences from previous point (slope) and gap
-        //           between floor and ceiling.
-        //           Need to take into account previous terrain to make sure the
-        //           terrain is passable: don’t create tunnels that are too
-        //           narrow or slopes that are too steep for the bat to maneuver.
-
-        if (difficulty == 0) {
-            // Create a nice random tunnel that is very wide.
-            // Note that difficulty is 0 before the game starts, so this should
-            // never collide with the bat that is flying at the center of the
-            // cave.
-            point.floorY = Random.Range(minFloor, minFloor + 0.5f);
-            point.ceilingY = Random.Range(maxCeiling - 0.5f, maxCeiling);
-        } else {
-            point.floorY = Random.Range(minFloor, maxCeiling - minGap);
-            point.ceilingY = Random.Range(point.floorY + minGap, maxCeiling);
+        if (!currentPattern.HasMorePoints()) {
+            Debug.Log("Reached end of pattern - " + currentPattern.name);
         }
-        // Choose the distance from the previous point.
-        point.distanceFromPrevious = Random.Range(minDistance, maxDistance);
+
+        var pointDetails = currentPattern.GetNextPointPrepare();
+        point.distanceFromPrevious = pointDetails.GetDistance();
+        point.ceilingY = pointDetails.GetCeilingY();
+        point.floorY = pointDetails.GetFloorY();
 
         // Set the point at the correct position based on the previous point's
         // position and the distance from it that we just set.
